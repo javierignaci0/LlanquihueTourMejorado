95056390dc17e33c96ab837d2e44d5ef63c1ce25
javierignaci0 <javierignacioruiza@gmail.com>
2026-06-07 12:50:25 -0400

    Fourth commit, arreglos de javadoc de Persona.java

--- src/model/Persona.java
+++ src/model/Persona.java
@@ -1,15 +1,26 @@
 package model;
 
+/**
+ * Representacion de una persona del sistema con nombre, apellido, rut, direccion y telefono
+ * Rut y Direccion son de tipo Composicion
+ */
 public class Persona {
-
     private String nombre;
     private String apellido;
+    // composicion - rut
     private Rut rut;
     // composicion - direccion
     private Direccion direccion;
     private int telefono;
 
-    // constructor
+    /**
+     *
+     * @param nombre Nombre de la persona
+     * @param apellido Apellido de la persona
+     * @param rut Rut de la persona
+     * @param direccion Direccion de la persona
+     * @param telefono Telefono de la persona
+     */
     public Persona(String nombre, String apellido, Rut rut, Direccion direccion, int telefono) {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -18,15 +29,15 @@ public class Persona {
         this.telefono = telefono;
     }
 
+    /**
+     *
+     * @return Retorna la informacion legible de los atributos de persona
+     */
     @Override
     public String toString(){
         return nombre + " " + apellido + ", Rut: " + rut + ", Direccion: " + direccion +  ", Telefono: " + telefono;
 
     }
-
-
-
-    // getter y setter
 
     public String getNombre() {
         return nombre;
